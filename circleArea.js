3179518bb31706a4f7bb2b7dffc10cc901489086
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-04-08 07:36:35 +0200

    Multiply the areas of two different circles of different radii

---
+++ circleArea.js
@@ -0,0 +1,12 @@
+function circleArea(r,PI=3.14){
+    return PI*r*r
+}
+
+function multiplication (a1, a2){
+    return a1*a2;
+}
+
+var area1 = circleArea(5);
+var area2 = circleArea(6);
+var result =multiplication(area1,area2)
+console.log(result)
