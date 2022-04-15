20178484f1732c74b14650f7715809c416afce84
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-04-15 04:12:13 +0200

    Using the forEach construct

---
+++ foreach.js
@@ -0,0 +1,7 @@
+const animals = ["cat" , "dog" , "bird", "horse"];
+  
+animals.forEach((value , index , array) => {
+  console.log('value: ', value );
+  console.log('value parametresinin aldığı index :', index );
+  console.log('array:' , array );
+});
